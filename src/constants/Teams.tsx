c8986a243a450f5dd104b967a9ece574bbdd8531
CrissWillCarry <cf93082@gmail.com>
2025-10-25 23:59:40 -0400

    Added teams images and list

---
+++ src/constants/Teams.tsx
@@ -0,0 +1,24 @@
+import type { Team } from "../objects/Team";
+
+export const TEAMS: Team[] = [
+    { id: 1, name: "Atlético-MG" },
+    { id: 2, name: "Bahia" },
+    { id: 3, name: "Botafogo" },
+    { id: 4, name: "Ceará" },
+    { id: 5, name: "Corinthians" },
+    { id: 6, name: "Cruzeiro" },
+    { id: 7, name: "Flamengo" },
+    { id: 8, name: "Fluminense" },
+    { id: 9, name: "Fortaleza" },
+    { id: 10, name: "Juventude" },
+    { id: 11, name: "Grêmio" },
+    { id: 12, name: "Internacional" },
+    { id: 13, name: "Mirassol" },
+    { id: 14, name: "Palmeiras" },
+    { id: 15, name: "RB Bragantino" },
+    { id: 16, name: "Santos" },
+    { id: 17, name: "São Paulo" },
+    { id: 18, name: "Sport" },
+    { id: 19, name: "Vasco" },
+    { id: 20, name: "Vitória" },
+];
