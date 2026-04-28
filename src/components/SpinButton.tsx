5df0505ef32784149185b89cd25be62c47e4c990
CrissWillCarry <cf93082@gmail.com>
2026-04-28 19:18:42 -0400

    Removendo tigrinho

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -6,12 +6,9 @@ const SpinButton: React.FC<{ onSpin: () => void }> = ({ onSpin }) => {
         <button
             onClick={onSpin}
             onMouseEnter={()=>playHover()}
-            className="mx-auto bg-red-600 text-amber-500 text-center font-mono py-2 px-6 sm:px-8 rounded-lg hover:cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out flex items-center justify-center text-lg sm:text-xl"
+            className="mx-auto bg-red-600 text-white text-center font-mono py-2 px-6 sm:px-8 rounded-lg hover:cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out flex items-center justify-center text-lg sm:text-xl"
         >
-            <span className="flex flex-row items-center">
-                <img src="/images/tigrinho.png" alt="Spin Icon" className="h-6 w-6 sm:h-10 sm:w-10 mr-3" />
-                Sortear
-            </span>
+        Sortear
         </button>
     );
 };
